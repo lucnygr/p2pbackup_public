f360c3ca8583af344021e04a2cbbb195dcd588b0
lucnygr <gl.github.com@lucny.at>
2022-04-10 23:28:38 +0200

    - added property to save database unencrypted

--- p2pbackup/application/src/main/java/at/lucny/p2pbackup/config/PersistenceConfig.java
+++ p2pbackup/application/src/main/java/at/lucny/p2pbackup/config/PersistenceConfig.java
@@ -10,8 +10,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.io.Console;
 import java.io.IOException;
 import java.util.HexFormat;
+import java.util.Scanner;
 
 @Configuration
 public class PersistenceConfig {
@@ -27,11 +29,27 @@ public class PersistenceConfig {
     public DataSource dataSource(@Value("${spring.datasource.url}") String databaseUrl, P2PBackupProperties p2PBackupProperties, CryptoService cryptoService) throws IOException {
         MACCheckingDataSource macCheckingDataSource = new MACCheckingDataSource(p2PBackupProperties, cryptoService);
 
-        String hexPassword = HexFormat.of().formatHex(cryptoService.getSecretKeyGenerator().generate(SALT_KDF_DATABASE_KEY).getEncoded());
-        String hexIv = HexFormat.of().formatHex(cryptoService.getSecretKeyGenerator().generate(SALT_KDF_DATABASE_IV, 128).getEncoded());
+        String dbUrl = databaseUrl;
+        if (p2PBackupProperties.getDatabase().getEncrypt()) {
+            String hexPassword = HexFormat.of().formatHex(cryptoService.getSecretKeyGenerator().generate(SALT_KDF_DATABASE_KEY).getEncoded());
+            String hexIv = HexFormat.of().formatHex(cryptoService.getSecretKeyGenerator().generate(SALT_KDF_DATABASE_IV, 128).getEncoded());
+            dbUrl = databaseUrl + ";crypt_key=" + hexPassword + ";crypt_iv=" + hexIv + ";crypt_type=AES/CBC/PKCS5Padding";
+        } else {
+            String yesNo = null;
+            Console console = System.console();
+            if (console != null) {
+                yesNo = console.readLine("The database will not be stored encrypted, this is a security risk. Continue (Y/N)?:");
+            } else {
+                System.out.println("The database will not be stored encrypted, this is a security risk. Continue (Y/N)?:");
+                Scanner scanner = new Scanner(System.in);
+                yesNo = scanner.nextLine();
+            }
+            if (!"Y".equalsIgnoreCase(yesNo)) {
+                throw new IllegalArgumentException("Abort startup because database would be unencrypted");
+            }
+        }
+        LOGGER.debug("database url: {}", databaseUrl);
         var dataSourceBuilder = DataSourceBuilder.create();
-        String dbUrl = databaseUrl + ";crypt_key=" + hexPassword + ";crypt_iv=" + hexIv + ";crypt_type=AES/CBC/PKCS5Padding";
-        LOGGER.trace("database url: {}", dbUrl);
         DataSource dataSource = dataSourceBuilder
                 .url(dbUrl)
                 .build();
