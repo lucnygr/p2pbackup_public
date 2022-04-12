ede9bf40d7b75a5eb685eb5a67f96e6528e5e4fa
lucnygr <gl.github.com@lucny.at>
2022-04-12 20:14:09 +0200

    - parallel upload of blocks to cloud-provider

--- p2pbackup/application/src/main/java/at/lucny/p2pbackup/upload/service/CloudUploadServiceImpl.java
+++ p2pbackup/application/src/main/java/at/lucny/p2pbackup/upload/service/CloudUploadServiceImpl.java
@@ -1,5 +1,6 @@
 package at.lucny.p2pbackup.upload.service;
 
+import at.lucny.p2pbackup.application.support.StopApplicationEvent;
 import at.lucny.p2pbackup.backup.support.BackupUtils;
 import at.lucny.p2pbackup.cloud.CloudStorageService;
 import at.lucny.p2pbackup.cloud.CloudStorageServiceProvider;
@@ -8,8 +9,10 @@ import at.lucny.p2pbackup.core.domain.CloudUpload;
 import at.lucny.p2pbackup.core.repository.BlockMetaDataRepository;
 import at.lucny.p2pbackup.core.repository.CloudUploadRepository;
 import at.lucny.p2pbackup.localstorage.service.LocalStorageService;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -19,11 +22,16 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 import org.springframework.validation.annotation.Validated;
 
+import javax.annotation.PreDestroy;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Validated
@@ -39,11 +47,27 @@ public class CloudUploadServiceImpl implements CloudUploadService {
 
     private final BlockMetaDataRepository blockMetaDataRepository;
 
+    private boolean shutdown;
+
     public CloudUploadServiceImpl(LocalStorageService localStorageService, CloudUploadRepository cloudUploadRepository, CloudStorageServiceProvider cloudStorageServiceProvider, BlockMetaDataRepository blockMetaDataRepository) {
         this.localStorageService = localStorageService;
         this.cloudUploadRepository = cloudUploadRepository;
         this.cloudStorageServiceProvider = cloudStorageServiceProvider;
         this.blockMetaDataRepository = blockMetaDataRepository;
+    }
+
+    @EventListener
+    public void onApplicationEvent(StopApplicationEvent event) {
+        this.shutdown();
+    }
+
+    @PreDestroy
+    public synchronized void shutdown() {
+        LOGGER.trace("begin shutdown()");
+
+        this.shutdown = true;
+
+        LOGGER.trace("end shutdown()");
     }
 
     @Override
@@ -69,6 +93,7 @@ public class CloudUploadServiceImpl implements CloudUploadService {
         }
     }
 
+    @SneakyThrows
     @Override
     public void uploadLocalBackupBlocks() {
         LOGGER.trace("begin uploadLocalBackupBlocks()");
@@ -84,46 +109,66 @@ public class CloudUploadServiceImpl implements CloudUploadService {
             return;
         }
 
-        LOGGER.info("prepare to upload up to {} blocks", totalNrOfCloudUploads);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        try {
 
-        // load 100 cloud-uploads per iteration and upload them. they are no longer found by the sql-query, so start again with 10 uploads from the beginning
-        Pageable pageRequest = PageRequest.of(0, 10, Sort.Direction.ASC, "id");
-        Page<CloudUpload> backupBlocks;
+            LOGGER.info("prepare to upload up to {} blocks", totalNrOfCloudUploads);
 
-        long nrOfProcessedUploads = 0;
+            // load 100 cloud-uploads per iteration and upload them. they are no longer found by the sql-query, so start again with 10 uploads from the beginning
+            Pageable pageRequest = PageRequest.of(0, 10, Sort.Direction.ASC, "id");
+            Page<CloudUpload> backupBlocks;
 
-        do {
-            backupBlocks = this.cloudUploadRepository.findAllByShareUrlIsNull(pageRequest);
-            List<CloudUpload> uploadsToDelete = new ArrayList<>();
+            long nrOfProcessedUploads = 0;
 
-            for (CloudUpload localBackupBlock : backupBlocks.getContent()) {
-                Optional<Path> pathToBlock = this.localStorageService.loadFromLocalStorage(localBackupBlock.getBlockMetaData().getId());
-                if (pathToBlock.isPresent()) {
-                    Path blockPath = pathToBlock.get();
-                    CloudStorageService cloudStorageService = this.pickRandomCloudStorageService().orElseThrow(() -> new IllegalStateException("no cloud storage provider configured"));
-                    cloudStorageService.upload(blockPath);
-                    String publicUrl = cloudStorageService.share(blockPath.getFileName().toString());
-                    LOGGER.debug("share url for data {} is {}", blockPath.getFileName(), publicUrl);
+            do {
+                backupBlocks = this.cloudUploadRepository.findAllByShareUrlIsNull(pageRequest);
+                List<CloudUpload> uploadsToDelete = new ArrayList<>();
+                List<CompletableFuture<Void>> futures = new ArrayList<>();
 
-                    localBackupBlock.setProviderId(cloudStorageService.getId());
-                    localBackupBlock.setShareUrl(publicUrl);
-                    this.cloudUploadRepository.save(localBackupBlock);
-                } else {
-                    LOGGER.warn("block {} should be distributed but is not available, request from other peers via distribution service", localBackupBlock.getBlockMetaData().getId());
-                    uploadsToDelete.add(localBackupBlock);
+                for (CloudUpload localBackupBlock : backupBlocks.getContent()) {
+                    Optional<Path> pathToBlock = this.localStorageService.loadFromLocalStorage(localBackupBlock.getBlockMetaData().getId());
+                    if (pathToBlock.isPresent()) {
+                        Path blockPath = pathToBlock.get();
+
+                        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+                            LOGGER.trace("beginning to upload block {}", blockPath.getFileName());
+                            CloudStorageService cloudStorageService = this.pickRandomCloudStorageService().orElseThrow(() -> new IllegalStateException("no cloud storage provider configured"));
+                            cloudStorageService.upload(blockPath);
+                            String publicUrl = cloudStorageService.share(blockPath.getFileName().toString());
+                            LOGGER.debug("share url for data {} is {}", blockPath.getFileName(), publicUrl);
+
+                            localBackupBlock.setProviderId(cloudStorageService.getId());
+                            localBackupBlock.setShareUrl(publicUrl);
+                            this.cloudUploadRepository.save(localBackupBlock);
+                        }, executorService);
+                        futures.add(future);
+                    } else {
+                        LOGGER.warn("block {} should be distributed but is not available, request from other peers via distribution service", localBackupBlock.getBlockMetaData().getId());
+                        uploadsToDelete.add(localBackupBlock);
+                    }
+
+                    nrOfProcessedUploads++;
                 }
 
-                nrOfProcessedUploads++;
-            }
-            LOGGER.info("processed {}/{} cloud-upload-entries", nrOfProcessedUploads, backupBlocks.getTotalElements());
-            if (!CollectionUtils.isEmpty(uploadsToDelete)) {
-                this.cloudUploadRepository.flush();
-                this.cloudUploadRepository.deleteAllInBatch(uploadsToDelete);
-            }
-        } while (backupBlocks.hasNext());
+                CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
 
-        if (nrOfProcessedUploads > 0) {
-            LOGGER.info("processed {} cloud-upload-entries total", nrOfProcessedUploads);
+                if (!CollectionUtils.isEmpty(uploadsToDelete)) {
+                    this.cloudUploadRepository.flush();
+                    this.cloudUploadRepository.deleteAllInBatch(uploadsToDelete);
+                }
+
+                allFutures.join();
+                LOGGER.info("processed {}/{} cloud-upload-entries", nrOfProcessedUploads, backupBlocks.getTotalElements());
+            } while (backupBlocks.hasNext() && !this.shutdown);
+
+            if (nrOfProcessedUploads > 0) {
+                LOGGER.info("processed {} cloud-upload-entries total", nrOfProcessedUploads);
+            }
+        } finally {
+            executorService.shutdown();
+            if (!executorService.awaitTermination(15, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
         }
         LOGGER.trace("end uploadLocalBackupBlocks");
     }
