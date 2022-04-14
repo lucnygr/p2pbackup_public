b73734649cbf87e00295262fdaff38cdc112240e
lucnygr <gl.github.com@lucny.at>
2022-04-14 12:32:56 +0200

    - new Configuraiton AsyncConfig to configure exception-handler for async-methods that return a void

--- p2pbackup/application/src/main/java/at/lucny/p2pbackup/P2PBackupApplicationConfiguration.java
+++ p2pbackup/application/src/main/java/at/lucny/p2pbackup/P2PBackupApplicationConfiguration.java
@@ -4,14 +4,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableJpaRepositories
-@EnableScheduling
-@EnableAsync
 @ComponentScan
 public class P2PBackupApplicationConfiguration {
 }
